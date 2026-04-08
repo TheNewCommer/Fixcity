915893fa7700e718144910d3e9a3739e0f725e32
TheNewCommer <151705896+TheNewCommer@users.noreply.github.com>
2026-04-08 19:46:26 +0530

    Delete frontend/src directory

--- frontend/src/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-const api = axios.create()
-api.interceptors.request.use(cfg => {
-  const t = localStorage.getItem('fmc_token')
-  if (t) cfg.headers['Authorization'] = `Bearer ${t}`
-  return cfg
-})
-export default api
